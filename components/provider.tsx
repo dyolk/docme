e2d2605fe331c0ad28f89ae7f036e8c9125677f1
Mr.KiNG <hang.j@foxmail.com>
2026-04-29 10:58:54 +0800

    :art: art: 样式美化

--- components/provider.tsx
+++ components/provider.tsx
@@ -3,6 +3,28 @@ import SearchDialog from '@/components/search';
 import { RootProvider } from 'fumadocs-ui/provider/next';
 import { type ReactNode } from 'react';
 
+const zhTranslations = {
+  search: '搜索',
+  searchNoResult: '未找到结果',
+  toc: '本页目录',
+  tocNoHeadings: '无标题',
+  lastUpdate: '最后更新于',
+  chooseLanguage: '选择语言',
+  nextPage: '下一页',
+  previousPage: '上一页',
+  chooseTheme: '主题',
+  editOnGithub: '在 GitHub 上编辑',
+};
+
 export function Provider({ children }: { children: ReactNode }) {
-  return <RootProvider search={{ SearchDialog }}>{children}</RootProvider>;
+  return (
+    <RootProvider
+      search={{ SearchDialog }}
+      i18n={{
+        translations: zhTranslations,
+      }}
+    >
+      {children}
+    </RootProvider>
+  );
 }
