0f81c3f45fc3da415dab8f55ae677fa26aba83ff
aucru <2838080432@qq.com>
2026-05-20 13:15:08 +0800

    feat: 脚本编写

---
+++ public/resources/scripts/mysql/utils/install_depence.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Author: Aucru@mail.dyolk.com
+# Date: 2026-05-19
+# Version: 1.0.0
+
+map_software_name() {
+  :
+}
+
+main() {
+  :
+}
+
+main "$@"
--- public/resources/scripts/mysql/utils/rsync.sh
+++ public/resources/scripts/mysql/utils/rsync.sh
@@ -5,18 +5,28 @@ set -euo pipefail
 # Date: 2026-05-15
 # Version: 1.0.0
 
-fn struct_remote_config() {
+struct_remote_config() {
   export REMOTE_SERVER_IP="${REMOTE_SERVER_IP:-127.0.0.1}"
   export REMOTE_SERVER_USER="${REMOTE_SERVER_USER:-root}"
   export REMOTE_SERVER_PASS="${REMOTE_SERVER_PASS:-}"
   export REMOTE_SERVER_DIR="${REMOTE_SERVER_DIR:-/tmp/dyolk/rsync/data/}"
 }
 
-fn struct_local_config() {
+struct_local_config() {
   export LOCAL_DIR="${LOCAL_DIR:-/tmp/dyolk/rsync/data/}"
   export LOG_FILE="${LOG_FILE:-/tmp/dyolk/rsync/logs/$(date '+%Y%m%d%H%M').log}"
 }
 
-fn judge_rsync_exist() {:}
+judge_rsync_exist() {
+  :
+}
 
-fn hash_check() {}
+hash_check() {
+  :
+}
+
+main() {
+  :
+}
+
+main "$@"
